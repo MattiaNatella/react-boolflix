a46773ded9f159efd7b66ffdfcb427e3023d5987
MattiaNatella <mattianatella@gmail.com>
2025-01-28 17:58:31 +0100

    [add] posters

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -13,12 +13,18 @@ const Card = ({ movie, tv }) => {
             (selectFlag(tv?.original_language) || `https://flagsapi.com/${tv?.original_language.toUpperCase()}/shiny/64.png`)
 
     }
+
+    function getPoster() {
+        return movie ? `https://image.tmdb.org/t/p/w342/${movie?.poster_path}` : `https://image.tmdb.org/t/p/w342/${tv?.poster_path}`
+    }
+
     return (
         <>
             <div className="card col-4">
                 <div className="card-header">
                     {movie?.title || tv?.original_name}
                 </div>
+                <img src={getPoster()} alt="poster image" />
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">{movie?.original_title || tv?.original_name}</li>
                     <li className="list-group-item">
