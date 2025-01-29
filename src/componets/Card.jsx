d795d21352ec0dfeeb9bc842f3bacd9bf645aa97
MattiaNatella <mattianatella@gmail.com>
2025-01-29 11:35:26 +0100

    [add] stars Rating

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -18,6 +18,25 @@ const Card = ({ movie, tv }) => {
         return movie ? `https://image.tmdb.org/t/p/w342/${movie?.poster_path}` : `https://image.tmdb.org/t/p/w342/${tv?.poster_path}`
     }
 
+    function getStars(vote) {
+        let fullStar = <i className="fa-solid fa-star fa-spin fa-spin-reverse" ></i >
+        const emptyStar = <i class="fa-regular fa-star"></i>
+        let starsRating = []
+        let ceilVote = Math.ceil(vote / 2)
+
+        console.log(ceilVote)
+        for (let i = 1; i <= 5; i++) {
+            if (starsRating.length < ceilVote) {
+                starsRating.push(fullStar)
+            } else {
+                starsRating.push(emptyStar)
+            }
+        }
+        return starsRating
+
+
+    }
+
     return (
         <>
             <div className="card col-4">
@@ -29,7 +48,7 @@ const Card = ({ movie, tv }) => {
                     <li className="list-group-item">{movie?.original_title || tv?.original_name}</li>
                     <li className="list-group-item">
                         <img src={getLanguageFlag()} alt={movie?.original_language || tv?.original_language} /></li>
-                    <li className="list-group-item">{movie?.vote_average || tv?.vote_average}</li>
+                    <li className="list-group-item">{getStars(movie?.vote_average || tv?.vote_average)}</li>
                 </ul>
             </div>
         </>
